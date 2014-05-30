cf4ddaddd75c609f65a7e05ab3a63baeb2aa849f
unknown <pixma38@gmail.com>
2014-05-30 17:31:53 +0530

    simple pyhton script added for now, which read from COM port , data push to an endpoint left, also to create an endpoint itself a task now left.

---
+++ script/msin.py
@@ -0,0 +1,46 @@
+import os
+import serial, time
+from serial.tools import list_ports
+
+
+def serial_ports():
+    """
+    Returns a generator for all available serial ports
+    """
+    if os.name == 'nt':
+        # windows
+        for i in range(256):
+            try:
+                s = serial.Serial(i)
+                s.close()
+                yield 'COM' + str(i + 1)
+            except serial.SerialException:
+                pass
+    else:
+        # unix
+        for port in list_ports.comports():
+            yield port[0]
+
+
+if __name__ == '__main__':
+    comList = list(serial_ports())
+    validComLocation = comList[len(comList) - 1]
+    ser = serial.Serial()
+    ser.port = validComLocation
+    ser.baudrate = 9600
+    ser.bytesize = serial.EIGHTBITS
+    ser.parity = serial.PARITY_NONE
+    ser.stopbits = serial.STOPBITS_ONE
+    ser.timeout = None
+    ser.xonxoff = False     #disable software flow control
+    ser.rtscts = False     #disable hardware (RTS/CTS) flow control
+    ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
+    ser.writeTimeout = 2     #timeout for write
+    ser.open()
+
+    if ser.isOpen():
+        while True:
+            response = ser.readline()
+            print("data read:" + str(response))
+
+
